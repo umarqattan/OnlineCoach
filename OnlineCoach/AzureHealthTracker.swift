a0bc254aca397f779fe539eeaa72fb20a25c4f67
umarqattan <umarqattan@ucla.edu>
2017-07-24 00:51:49 -0700

    Add: Health Tracker Group

---
+++ OnlineCoach/AzureHealthTracker.swift
@@ -0,0 +1,12 @@
+//
+//  AzureHealthTracker.swift
+//  OnlineCoach
+//
+//  Created by Umar Qattan on 7/24/17.
+//  Copyright © 2017 Umar Qattan. All rights reserved.
+//
+
+import Foundation
+
+
+// Put your stuff here John... ;) 
